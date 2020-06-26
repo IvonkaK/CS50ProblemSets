a150b9ac4b5126f332e15a920b7e5b3293fd77b3
iwona <ivona.kuzia@gmail.com>
2020-06-26 18:01:15 +0200

    dictionary - week 5 corrected & finished

--- c/dictionary.c
+++ c/dictionary.c
@@ -1,13 +1,18 @@
 // Implements a dictionary's functionality
 
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <ctype.h>
 
 #include "dictionary.h"
 
 // Represents a node in a hash table
 typedef struct node
 {
-    char word[LENGTH + 1];
+    char word[LENGTH];
     struct node *next;
 }
 node;
@@ -24,7 +29,26 @@ int total = 0;
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
+    //set the cursor which will become the pointer to elements of our linked list
+    node *cursor = table[hash(word)];
+
+    //look for the word and return true if found
+    if (strcasecmp(cursor->word, word) == 0)
+    {
+        return true;
+    }
+
+    //while not reaching the end of linked list
+    while (cursor->next != NULL)
+    {
+        //take cursor and move to the next node of the linked list
+        cursor = cursor->next;
+
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
@@ -43,26 +67,40 @@ bool load(const char *dictionary)
     FILE *file = fopen(dictionary, "r");
 
     //init temp space for holding the word
-    char *strWord = malloc(LENGHT);
-    if (word == NULL){ return false; }
+    char *strWord = malloc(LENGTH);
+    if (strWord == NULL)
+    {
+        return false;
+    }
 
     //while not reaching end of file
     while (fscanf(file, "%s", strWord) != EOF)
     {
         //allocate memory for a node in which I insert the word
         node *n = malloc(sizeof(node));
-        if (n == NULL){ return false; }
+        if (n == NULL)
+        {
+            return false;
+        }
 
         //copy the word into allocated node space
         strcpy(n->word, strWord);
-        //count the word
-        total ++;
+        //count the words
+        total++;
 
+        //next has to point to the beginning of the list
+        n->next = table[hash(strWord)];
+
+        //list has to point at n (n is the head of the list)
+        table[hash(strWord)] = n;
 
     }
 
+    //close the file and free the memory for strWord
+    fclose(file);
+    free(strWord);
 
-    return false;
+    return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
@@ -75,6 +113,33 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    //set the cursor which will become the pointer to elements of our linked list
+    node *cursor;
+    //temp pointer
+    node *temp;
+
+    //iterate every bucket in the table
+    for (int i = 0; i < N; i++)
+    {
+        //if the current bucket of the table is empty go to the next one
+        if (table[i] == NULL)
+        {
+            continue;
+        }
+
+        //set cursor to current linked list and temp value to cursor so they would both point to the same node of the linked list
+        cursor = table[i];
+        temp = cursor;
+
+        //while not reaching end of linked list free allocated memory for each node
+        while (cursor->next != NULL)
+        {
+            cursor = cursor->next;
+            free(temp);
+            temp = cursor;
+        }
+
+        free(cursor);
+    }
+    return true;
 }
